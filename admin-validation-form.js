e45d37ae946f648b0d1c022e0fff64504a33b288
ZooM982 <haurlyroll@gmail.com>
2024-01-06 11:38:31 +0000

    ajout de firebase

--- admin-validation-form.js
+++ admin-validation-form.js
@@ -21,48 +21,38 @@ function validateForm(){
 }
 
 
-function LogIn(){
-    if(validateForm() == true){
-        window.location.href="admin-dashboard.html";    
-    }
-}
+// var add = document.getElementById('addToDo');
+// var input = document.getElementById('new-category');
+// var CategorieAdd = document.getElementById('CategorieAdd');
 
-function LogOut(){
-    window.location.href="admin-login.html"
-}
-
-var add = document.getElementById('addToDo');
-var input = document.getElementById('new-category');
-var CategorieAdd = document.getElementById('CategorieAdd');
-
-add.addEventListener('click',addItem);
-input.addEventListener('keypress',function(e){
-    if(e.key=="Enter"){
-        addItem();
-    }
-});
-function addItem(e){
+// add.addEventListener('click',addItem);
+// input.addEventListener('keypress',function(e){
+//     if(e.key=="Enter"){
+//         addItem();
+//     }
+// });
+// function addItem(e){
   
-  const item_value  = input.value;
-  const item = document.createElement('div');
-		item.classList.add('item');
+//   const item_value  = input.value;
+//   const item = document.createElement('div');
+// 		item.classList.add('item');
 
-		const item_content = document.createElement('div');
-		item_content.classList.add('content');
+// 		const item_content = document.createElement('div');
+// 		item_content.classList.add('content');
 
-		item.appendChild(item_content);
+// 		item.appendChild(item_content);
 
-		const input_item = document.createElement('input');
-		input_item.classList.add('text');
-		input_item.type = 'text';
-		input_item.value = item_value;
-		input_item.setAttribute('readonly', 'readonly');
-        input_item.addEventListener('dblclick', function(){
-            input_item.style.textDecoration = "line-through";
-        })
-        item_content.appendChild(input_item);
+// 		const input_item = document.createElement('input');
+// 		input_item.classList.add('text');
+// 		input_item.type = 'text';
+// 		input_item.value = item_value;
+// 		input_item.setAttribute('readonly', 'readonly');
+//         input_item.addEventListener('dblclick', function(){
+//             input_item.style.textDecoration = "line-through";
+//         })
+//         item_content.appendChild(input_item);
 
-		CategorieAdd.appendChild(item);
+// 		CategorieAdd.appendChild(item);
 
-		input.value = '';
-}
+// 		input.value = '';
+// }
